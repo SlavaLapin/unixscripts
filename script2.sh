fe927bd5e950bd50c9b1734bd78b477442f7daa4
Slava <vechyaslaugh@gmail.com>
2018-09-26 22:57:06 +0300

    Made script2.sh, not tested yet

--- script2.sh
+++ script2.sh
@@ -1,56 +1,52 @@
-cd lab0
+cd ~/lab0
 
-chmod u=wx darumaka2
-chmod g=rwx darumaka2
-chmod o=rx darumaka2
+chmod u=rx bellsprout2
+chmod g=rwx bellsprout2
+chmod o=rw bellsprout2
 
-chmod 006 darumaka2/klang
+chmod a= bellsprout2/dratini
+chmod u=rw
 
-chmod a= darumaka2/shelmet
-chmod u=r darumaka2/shelmet
-chmod g=u darumaka2/shelmet
+chmod a=r bellsprout2/panpour
 
-chmod 373 darumaka2/machop
+chmod 046 bellsprout2/nidoranF
 
-chmod 317 darumaka2/rhyperior
+chmod a=wx empoleon2
 
-chmod a= darumaka2/zweilous
-chmod u+r darumaka2/zweilous
-chmod g=u darumaka2/zweilous
+chmod a=r empoleon2/metang
+chmod g+w empoleon2/metang
+chmod u+x empoleon/metang
 
-chmod 004 kingdra2
+chmod 311 empoleon2/medicham
 
-chmod 666 krokodile5
+chmod 600 empoleon2/amoonguss
 
-chmod a= meowth1
-chmod u+r meowth1
+chmod 357 empoleon2/bastiodon
+
+chmod 444 empoleon2/milotic
+
+chmod 064 magmortar5
+
+chmod a= murkrow1
+chmod g=w murkrow1
+chmod u+g murkrow1
+chmod u+r murkrow1
+
+chmod 577 palpitoad1
+
+chmod 404 palpitoad1/caterpie
+
+chmod a=r palpitoad1/volcarona
 
 
-chmod a=wx porygonz1
-chmod u+r porygonz1
+chmod u=rwx palpitoad1/steelix
+chmod o=w palpitoad1/steelix
+chmod g-o palpitoad1/steelix
 
-chmod 551 porygonz1/sceptile
+chmod a=r palpitoad1/axew
+chmod g-r pa1pitoad1/axew
 
-chmod 576 porygonz1/haunter
-
-chmod 577 porygonz1/bronzong
-
-chmod 666 porygonz1/charmeleon
-
-chmod a=rwx shieldon7
-chmod g-w shieldon7
-chmod o-r shieldon7
-
-chmod a=r shieldon7/herdier
-
-chmod a=x shieldon7/dragonair
-chmod u-w shieldon7/dragonair
-chmod g-r shieldon7/dragonair
-
-chmod 550 shieldon7/mudkip
-
-chmod 363 shieldon7/chansey
-
-chmod 307 shieldon7/swalot
+chmod a= snivy7
+chmod uo+r snivy7
 
 cd ..
