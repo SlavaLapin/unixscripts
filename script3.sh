60672d3e4daacb08064314cb336fca5d8eae2055
SlavaLapin <vechyaslaugh@gmail.com>
2018-09-27 01:43:45 +0300

    script3.sh - Final version, works, has all of the output mentioned in the comments. It does what is asked, fails, makes necessary changes, and does it again, finally succeeding

--- script3.sh
+++ script3.sh
@@ -2,81 +2,54 @@
 
 cd ~/lab0
 
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
-
 #cоздать символическую ссылку для файла murkrow1 с именем lab0/palpitoad1/axewmurkrow
 
 ln -s murkrow1 ./palpitoad1/axewmurkrow
-
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
+# ln: cannot create ./palpitoad1/axewmurkrow: Permission denied
+# no permission to write in palpitoad1
+chmod u+w ~/lab0/palpitoad1
+ln -s murkrow1 ./palpitoad1/axewmurkrow
+# Works!
 
 #скопировать рекурсивно директорию empoleon2 в директорию lab0/empoleon2/bastiodon
 
-cp -R empoleon2 ./empoleon2/bastiodon
-
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
+#cp -R empoleon2 ./empoleon2/bastiodon
+# cp: empoleon2: Permission denied
+# no permission to read empoleon2
+# chmod -R u+r ~/lab0/empoleon2
+# cp: ./empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2/bastiodon/empoleon2: File name too long
+# This operation is impossible
 
 #создать символическую ссылку c именем Copy_85 на директорию palpitoad1 в каталоге lab0
 
 ln -s palpitoad1 Copy_85
-
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
+# Just works! Finally!
 
 #скопировать содержимое файла murkrow1 в новый файл lab0/bellsprout2/dratinimurkrow
 
 cp murkrow1 ./bellsprout2/dratinimurkrow
-
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
+# cp: cannot create ./bellsprout2/dratinimurkrow: Permission denied
+# no permission to write in bellsprout2
+chmod u+w ~/lab0/bellsprout2
+cp murkrow1 ./bellsprout2/dratinimurkrow
+# Works!
 
 #cоздать жесткую ссылку для файла magmortar5 с именем lab0/palpitoad1/axewmagmortar
 
 ln magmortar5 ./palpitoad1/axewmagmortar
-
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
+# Works!
 
 #скопировать файл magmortar5 в директорию lab0/empoleon2/medicham
 
 cp magmortar5 ./empoleon2/medicham
-
-read -p 'Any Errors? ' ervar
-if [ $ervar -gt 0 ]
-	then
-	echo 'Fix them'
-	exit 12113
-fi
+# cp: cannot open magmortar5: Permission denied
+# No permission to read magmortar5
+chmod u+r ~/lab0/magmortar5
+cp magmortar5 ./empoleon2/medicham
 
 #объеденить содержимое файлов lab0/bellsprout2/dratini, lab0/palpitoad1/axew, в новый файл lab0/murkrow1_94
 
 cat ./bellsprout2/dratini ./palpitoad1/axew > murkrow1_94
+# Works!
 
 cd ..
