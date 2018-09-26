d1a3cfea6a5516841ee3b7e76c25c8e517a54d2a
SlavaLapin <vechyaslaugh@gmail.com>
2018-09-26 23:45:17 +0300

    Made script1.sh & script2.sh more verbose. FIX-ATTEMPT for script2.sh

--- script1.sh
+++ script1.sh
@@ -6,6 +6,7 @@ mkdir bellsprout2
 touch bellsprout2/dratini
 touch bellsprout2/panpour
 touch bellsprout2/nidoranF
+echo 'bellsprout2 & contents created \n'
 
 mkdir empoleon2
 mkdir empoleon2/metang
@@ -13,46 +14,61 @@ mkdir empoleon2/medicham
 touch empoleon2/amoonguss
 mkdir empoleon2/bastiodon
 touch empoleon2/milotic
+echo 'empoleon2 & contents created \n'
 
 touch magmortar5
 touch murkrow1
+echo 'magmortar5 & murkrow1 created \n'
 
 mkdir palpitoad1
 touch palpitoad1/caterpile
 touch palpitoad1/volcarona
 mkdir palpitoad1/steelix
 touch palpitoad1/axew
+echo 'palpitoad & contents created \n'
 
 touch snivy7
+echo 'snivy7  & all files created \n'
 
 echo 'satk=5 sdef=5 spd=5' > bellsprout2/dratini
+echo 'dratini set \n'
 
 echo 'Развитые способности
 Prankster' > bellsprout2/panpour
+echo 'panpour set \n'
 
 echo 'Способности Growl Scratch Tail Whip Double Kick
 Poison Sting Fury Swipes Bite Helping Hand Toxic Spikes Flatter Crunch
 Captivate Poison Fang' > bellsprout2/nidoranF
+echo 'nidoranF set \n'
 
 echo 'weight=23.1 height=24.0 atk=9
 def=7' > empoleon2/amoonguss
+echo 'amoonguss set \n'
 
 echo 'Способности Torrent Marvel Scale Swift
 Swim' > empoleon2/milotic
+echo 'milotic set \n'
 
 echo 'Развитые способности Vital
 Spirit' > magmortar5
+echo 'magmortar5 set \n'
 
 echo 'weigth=4.6 height=20.0 atk=9 def=4' > murkrow1
+echo 'murkrow1 set \n'
 
 echo 'Тип
 покемона BUG NONE' > palpitoad1/caterpie
+echo 'caterpie set \n'
 
 echo 'satk=14 sdef=11 spd=10' > palpitoad1/volcarona
+echo 'volcarona set \n'
 
 echo 'satk=3
 sdef=4 spd=6' > palpitoad1/axew
+echo 'axew set \n'
 
 echo 'Тип диеты Herbivore' > snivy7
+echo 'snivy7 set \n'
 
 cd ..
--- script2.sh
+++ script2.sh
@@ -3,50 +3,68 @@ cd ~/lab0
 chmod u=rx bellsprout2
 chmod g=rwx bellsprout2
 chmod o=rw bellsprout2
+echo 'Permissions for bellsprout2 set \n'
 
 chmod a= bellsprout2/dratini
-chmod u=rw
+chmod u=rw bellsprout2/dratini
+echo 'Permissions for dratini set \n'
 
 chmod a=r bellsprout2/panpour
+echo 'Permissions for panpour set \n'
 
 chmod 046 bellsprout2/nidoranF
+echo 'Permissions for nidoranF set \n'
 
 chmod a=wx empoleon2
+echo 'Permissions for empoleon2 set \n'
 
 chmod a=r empoleon2/metang
 chmod g+w empoleon2/metang
-chmod u+x empoleon/metang
+chmod u+x empoleon2/metang
+echo 'Permissions for metang set \n'
 
 chmod 311 empoleon2/medicham
+echo 'Permissions for medicham set \n'
 
 chmod 600 empoleon2/amoonguss
+echo 'Permissions for amoonguss set \n'
 
 chmod 357 empoleon2/bastiodon
+echo 'Permissions for bastiodon set \n'
 
 chmod 444 empoleon2/milotic
+echo 'Permissions for milotic set \n'
 
 chmod 064 magmortar5
+echo 'Permissions for magmortar5 set \n'
 
 chmod a= murkrow1
 chmod g=w murkrow1
 chmod u+g murkrow1
 chmod u+r murkrow1
+echo 'Permissions for murkrow1 set \n'
 
 chmod 577 palpitoad1
+echo 'Permissions for palpitoad1 set \n'
 
 chmod 404 palpitoad1/caterpie
+echo 'Permissions for caterpie set \n'
 
 chmod a=r palpitoad1/volcarona
+echo 'Permissions for volcarona set \n'
 
 
 chmod u=rwx palpitoad1/steelix
 chmod o=w palpitoad1/steelix
 chmod g-o palpitoad1/steelix
+echo 'Permissions for steelix set \n'
 
 chmod a=r palpitoad1/axew
 chmod g-r pa1pitoad1/axew
+echo 'Permissions for axew set \n'
 
 chmod a= snivy7
 chmod uo+r snivy7
+echo 'Permissions for snivy7 set \n'
 
 cd ..
